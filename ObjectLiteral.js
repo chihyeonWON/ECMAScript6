e786e8cbce76b94ece250098404b6d67f191ab75
원치현 <heungeob1003@gmail.com>
2021-09-25 20:33:41 +0900

    Computed Property names

---
+++ ObjectLiteral.js
@@ -0,0 +1,5 @@
+var prop = "name", i=1;
+var obj = { [prop+i]: "Tom"}
+console.log(obj); //-> Object {name1:"Tom"}
+var obj = { [Symbol("heart")]: "A"};
+console.log(obj); //-> Object {Symbol(heart):"A"}
--- SafeExtension.js
+++ SafeExtension.js
@@ -4,7 +4,9 @@ Array.prototype[Symbol.for("shuffle")] = function() {
     while(m) {
         // m이 0이 되면 멈춘다
         i = Math.floor(Math.random()*m--); // m 미만의 인덱스 i를 무작위로 골라서 하나씩 삭제한다
-        t = a[m]; a[m] = a[i]; a[i] = t; // a[i]와 a[m]을 교환한다
+        t = a[m]; 
+        a[m] = a[i]; 
+        a[i] = t; // a[i]와 a[m]을 교환한다
     }
     return this;
 };
